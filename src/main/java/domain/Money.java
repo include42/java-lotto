5e8906cae7c8428636115ebd0aa83d661cfc3180
gunhee lee <dlrjsgml42@naver.com>
2020-02-24 14:38:58 +0900

    [feat] 반복 횟수 저장을 위한 RepeatCount 클래스 구현
    RepeatCount 반복 횟수를 저장하고 반복 종료 여부를 체크하는 클래스 구현
    RepeatCountTest RepeatCount의 생성, 예외, 반복 종료 확인 기능 테스트
    Money 금액에 따른 로또 게임 횟수를 RepeatCount로 반환하도록 수정
    ALL 구조 변경에 따른 코드 리팩토링

--- src/main/java/domain/Money.java
+++ src/main/java/domain/Money.java
@@ -29,7 +29,7 @@ public class Money {
         return 100 * earningMoney / money;
     }
 
-    public int calculateGames() {
-        return money / LOTTO_PRICE;
+    public RepeatCount calculateGames() {
+        return new RepeatCount(money / LOTTO_PRICE);
     }
 }
